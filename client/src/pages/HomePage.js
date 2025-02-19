2008bb43bc346989493125aaf8656cfdfc4854bf
Vipul Kumar <143587680+Vipul20K@users.noreply.github.com>
2025-02-19 23:16:02 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -8,6 +8,7 @@ import { Table } from "antd";
 import moment from 'moment';
 import Analytics from '../components/Analytics';
 import '../styles/HomePageStyles.css';
+const API_URL = process.env.REACT_APP_API_URL;
 // import axiosInstance from '../utils/axiosInstance';
 
 
@@ -82,7 +83,7 @@ const getAllTransactions = useCallback(async () => {
     setLoading(true);
 
     // Add the token to the Authorization header as a Bearer token
-    const res = await axios.post('api/v1/transactions/get-transaction', {
+    const res = await axios.post('${API_URL}/api/v1/transactions/get-transaction', {
       userid: user._id,
       frequency,
       selectedDate,
@@ -122,7 +123,7 @@ const handleDelete=async(record)=>{
     }
 
     setLoading(true);
-    await axios.post('api/v1/transactions/del-transaction',{transactionId:record._id}
+    await axios.post('${API_URL}/api/v1/transactions/del-transaction',{transactionId:record._id}
       ,{
         headers:{
           'Authorization':`Bearer ${token}`,
@@ -162,7 +163,7 @@ const handleDelete=async(record)=>{
       setLoading(true);
 
       if (editable) {
-        await axios.post("api/v1/transactions/edit-transaction", {
+        await axios.post("${API_URL}/api/v1/transactions/edit-transaction", {
           payload: {
             ...values,
             userId: user._id,
